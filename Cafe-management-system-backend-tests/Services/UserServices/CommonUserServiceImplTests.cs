629b5582c760c5d305c49b08e8a7e920b76a6e78
George Karanasios <kar.giorgos7@hotmail.com>
2023-12-29 11:57:16 +0200

    Add Unit Tests for Services
    1. CommonUserService unit testing
    2. UserService unit testing

---
+++ Cafe-management-system-backend-tests/Services/UserServices/CommonUserServiceImplTests.cs
@@ -0,0 +1,111 @@
+﻿using Cafe_management_system_backend.MVC.Models;
+using Cafe_management_system_backend.MVC.Repositories;
+using Cafe_management_system_backend.MVC.Services.UserServices;
+using Moq;
+using AutoFixture;
+
+namespace Cafe_management_system_backend_tests.UserServices
+{
+
+    [TestClass]
+    public class CommonUserServiceImplTests
+    {
+        private Mock<UserRepository> mockUserRepository;
+        private CommonUserServiceImpl commonUserServiceImpl;
+        private Fixture fixture;
+        private User userDB;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockUserRepository = new Mock<UserRepository>();
+            commonUserServiceImpl = new CommonUserServiceImpl(mockUserRepository.Object);
+            fixture = new Fixture(); // Create an instance of Fixture in the setup method or constructor
+            userDB = fixture.Create<User>();
+        }
+
+        [TestMethod]
+        public void FindAllUsers_ShouldReturnListOfUsers()
+        {
+            // Arrange/Given
+            var expectedUsers = fixture.CreateMany<User>(5).ToList();
+            mockUserRepository.Setup(repo => repo.FindAll()).Returns(expectedUsers);
+            // Act/When
+            var result = commonUserServiceImpl.FindAllUsers();
+            // Assert/Then
+            CollectionAssert.AreEqual(expectedUsers, result);
+        }
+
+        [TestMethod]
+        public void FindUserById_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            mockUserRepository.Setup(repo => repo.FindById(userDB.id)).Returns(userDB);
+            // Act
+            var result = commonUserServiceImpl.FindUserById(userDB.id);
+            // Assert
+            Assert.AreEqual(userDB, result);
+            //mockLogger.Verify(logger => logger.Info($"[UserService:FindUserWithLogs()] Success: User was successfully found (Id: {userDB.id} & Email: {userDB.email})"), Times.Once);
+        }
+
+        [TestMethod]
+        public void FindUserById_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            int userId = 0;
+            mockUserRepository.Setup(repo => repo.FindById(userId)).Returns((User)null);
+            // Act
+            var result = commonUserServiceImpl.FindUserById(userId);
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FindUserByEmail_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            mockUserRepository.Setup(repo => repo.FindByEmail(userDB.email)).Returns(userDB);
+            // Act
+            var result = commonUserServiceImpl.FindUserByEmail(userDB.email);
+            // Assert
+            Assert.AreEqual(userDB, result);
+        }
+
+        [TestMethod]
+        public void FindUserByEmail_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userEmail = "nonexistent@example.com";
+            mockUserRepository.Setup(repo => repo.FindByEmail(userEmail)).Returns((User)null);
+            // Act
+            var result = commonUserServiceImpl.FindUserByEmail(userEmail);
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FindUserByEmailAndPassword_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(userDB.email, userDB.password)).Returns(userDB);
+            // Act
+            var result = commonUserServiceImpl.FindUserByEmailAndPassword(userDB.email, userDB.password);
+            // Assert
+            Assert.AreEqual(userDB, result);
+        }
+
+        [TestMethod]
+        public void FindUserByEmailAndPassword_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userEmail = "nonexistent@example.com";
+            var userPassword = "password";
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(userEmail, userPassword)).Returns((User)null);
+            // Act
+            var result = commonUserServiceImpl.FindUserByEmailAndPassword(userEmail, userPassword);
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+
+}
---
+++ Cafe-management-system-backend-tests/Services/UserServices/UserServiceImplTests.cs
@@ -0,0 +1,226 @@
+﻿using Moq;
+using AutoFixture;
+using Cafe_management_system_backend.MVC.Models;
+using Cafe_management_system_backend.MVC.Repositories;
+using Cafe_management_system_backend.MVC.Security;
+using Cafe_management_system_backend.MVC.Services.UserServices;
+using System.Data;
+
+namespace Cafe_management_system_backend_tests.UserServices
+{
+    [TestClass]
+    public class UserServiceImplTests
+    {
+        private Mock<UserRepository> mockUserRepository;
+        private Mock<CommonUserService> mockCommonUserService;
+        private UserServiceImpl userServiceImpl;
+        private Fixture fixture;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockUserRepository = new Mock<UserRepository>();
+            mockCommonUserService = new Mock<CommonUserService>();
+            userServiceImpl = new UserServiceImpl(mockUserRepository.Object, mockCommonUserService.Object);
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void SignUp_ShouldCreateUser_WhenEmailDoesNotExist()
+        {
+            // Arrange
+            var newUser = fixture.Create<User>();
+            newUser.email = "newuser@example.com";
+
+            mockUserRepository.Setup(repo => repo.FindByEmail(newUser.email)).Returns((User)null);
+
+            // Act
+            userServiceImpl.SignUp(newUser);
+
+            // Assert
+            mockUserRepository.Verify(repo => repo.AddUser(newUser), Times.Once);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DuplicateNameException))]
+        public void SignUp_ShouldThrowException_WhenEmailExists()
+        {
+            // Arrange
+            var existingUser = fixture.Create<User>();
+            existingUser.email = "existinguser@example.com";
+
+            mockUserRepository.Setup(repo => repo.FindByEmail(existingUser.email)).Returns(existingUser);
+
+            // Act
+            userServiceImpl.SignUp(existingUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SignUp_ShouldThrowException_WhenUserEmailIsNull()
+        {
+            // Arrange
+            var userWithNullEmail = fixture.Create<User>();
+            userWithNullEmail.email = null;
+
+            // Act
+            userServiceImpl.SignUp(userWithNullEmail);
+
+            // Assert
+            // The ExpectedException attribute is used to specify that the test method
+            // is expected to throw the specified exception type (Exception) during its execution.
+        }
+
+
+        [TestMethod]
+        public void Login_ShouldGenerateToken_WhenCredentialsAreCorrect()
+        {
+            // Arrange
+            var loginUser = fixture.Create<User>();
+            loginUser.email = "user@example.com";
+            loginUser.password = "password";
+
+            var userDB = fixture.Create<User>();
+            userDB.status = "true"; // Active user
+
+            // Set up the UserRepository mock
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(loginUser.email, loginUser.password)).Returns(userDB);
+
+            // Set up the CommonUserService mock
+            mockCommonUserService.Setup(service => service.FindUserByEmailAndPassword(loginUser.email, loginUser.password)).Returns(userDB);
+
+            // Act
+            var result = userServiceImpl.Login(loginUser);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            // Verify that the UserRepository mock was called
+            mockUserRepository.Verify(repo => repo.FindByEmailAndPassword(loginUser.email, loginUser.password), Times.Once);
+
+            // Verify that the CommonUserService mock was called
+            //mockCommonUserService.Verify(service => service.FindUserByEmailAndPassword(loginUser.email, loginUser.password), Times.Once);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(UnauthorizedAccessException))]
+        public void Login_ShouldThrowUnauthorizedException_WhenUserStatusIsInactive()
+        {
+            // Arrange
+            var inactiveUser = fixture.Create<User>();
+            inactiveUser.status = "false"; // Inactive user
+
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(inactiveUser.email, inactiveUser.password)).Returns(inactiveUser);
+
+            // Act
+            userServiceImpl.Login(inactiveUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnauthorizedAccessException))]
+        public void Login_ShouldThrowUnauthorizedException_WhenUserNotFound()
+        {
+            // Arrange
+            var loginUser = fixture.Create<User>();
+            loginUser.email = "nonexistent@example.com";
+
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(loginUser.email, loginUser.password)).Returns((User)null);
+
+            // Act
+            userServiceImpl.Login(loginUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnauthorizedAccessException))]
+        public void Login_ShouldThrowUnauthorizedException_WhenUserStatusIsPending()
+        {
+            // Arrange
+            var pendingUser = fixture.Create<User>();
+            pendingUser.status = "pending"; // Pending user
+
+            mockUserRepository.Setup(repo => repo.FindByEmailAndPassword(pendingUser.email, pendingUser.password)).Returns(pendingUser);
+
+            // Act
+            userServiceImpl.Login(pendingUser);
+        }
+
+        [TestMethod]
+        public void UpdateUser_ShouldUpdateUser_WhenUserExists()
+        {
+            // Arrange
+            var updateUser = fixture.Create<User>();
+            var updatedUserDB = fixture.Create<User>();
+
+            mockCommonUserService.Setup(service => service.FindUserById(updateUser.id)).Returns(updatedUserDB);
+
+            // Act
+            var result = userServiceImpl.UpdateUser(updateUser);
+
+            // Assert
+            Assert.AreEqual(updatedUserDB, result);
+            mockUserRepository.Verify(repo => repo.UpdateUser(updatedUserDB), Times.Once);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void UpdateUser_ShouldThrowException_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var updateUser = fixture.Create<User>();
+
+            mockCommonUserService.Setup(service => service.FindUserById(updateUser.id)).Returns((User)null);
+
+            // Act
+            userServiceImpl.UpdateUser(updateUser);
+        }
+
+        [TestMethod]
+        public void ChangeUserPassword_ShouldChangePassword_WhenOldPasswordIsCorrect()
+        {
+            // Arrange
+            var principal = fixture.Create<PrincipalProfile>();
+            var changePassword = fixture.Create<ChangePassword>();
+            var userDB = fixture.Create<User>();
+
+            mockCommonUserService.Setup(service => service.FindUserByEmailAndPassword(principal.Email, changePassword.oldPassword)).Returns(userDB);
+
+            // Act
+            var result = userServiceImpl.ChangeUserPassword(principal, changePassword);
+
+            // Assert
+            Assert.AreEqual(userDB, result);
+            mockUserRepository.Verify(repo => repo.UpdateUser(userDB), Times.Once);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ChangeUserPassword_ShouldThrowException_WhenOldPasswordIsIncorrect()
+        {
+            // Arrange
+            var principal = fixture.Create<PrincipalProfile>();
+            var changePassword = fixture.Create<ChangePassword>();
+
+            mockCommonUserService.Setup(service => service.FindUserByEmailAndPassword(principal.Email, changePassword.oldPassword)).Returns((User)null);
+
+            // Act
+            userServiceImpl.ChangeUserPassword(principal, changePassword);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ChangeUserPassword_ShouldThrowException_WhenNewPasswordIsNull()
+        {
+            // Arrange
+            var principal = fixture.Create<PrincipalProfile>();
+            var changePassword = fixture.Create<ChangePassword>();
+            var userDB = fixture.Create<User>();
+
+            mockCommonUserService.Setup(service => service.FindUserByEmailAndPassword(principal.Email, changePassword.oldPassword)).Returns(userDB);
+
+            // Act
+            userServiceImpl.ChangeUserPassword(principal, new ChangePassword { oldPassword = "old", newPassword = null });
+        }
+
+    }
+}
