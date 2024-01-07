e82d2b557f504efcb6d654f4c57786360293f595
George Karanasios <kar.giorgos7@hotmail.com>
2024-01-07 23:29:35 +0200

    2nd Frontend Project Setup (Video guide files)
    Angular and npm versions/packages had to be downgraded

--- Frontend-Cafe-Management-System/src/app/app.component.spec.ts
+++ Frontend-Cafe-Management-System/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Frontend-Cafe-Management-System' title`, () => {
+  it(`should have as title 'Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend-Cafe-Management-System');
+    expect(app.title).toEqual('Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Frontend-Cafe-Management-System');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('Frontend app is running!');
   });
 });
