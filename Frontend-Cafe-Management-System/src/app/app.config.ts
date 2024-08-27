252b8083c35413709bc06993c6d43a630f2f4401
George Karanasios <kar.giorgos7@hotmail.com>
2024-08-27 13:04:22 +0300

    Use  'provideRoutes' for configuring routes (Angular 11)

--- Frontend-Cafe-Management-System/src/app/app.config.ts
+++ Frontend-Cafe-Management-System/src/app/app.config.ts
@@ -1,8 +1,9 @@
+// @ts-ignore
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRoutes } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRoutes(routes)]
 };
